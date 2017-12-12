dd5035155e917224b254a175fcb021f9e528d0af
platane <me@platane.me>
2017-12-12 15:58:52 +0100

    :hammer: fix publish all cmd

--- back/src/module/publishAllBatch/cmd.js
+++ back/src/module/publishAllBatch/cmd.js
@@ -1,4 +1,4 @@
-const start_date = process.argv[1] ? new Date(process.argv[1]).getTime() : null
-const end_date = process.argv[2] ? new Date(process.argv[2]).getTime() : null
+const start_date = process.argv[2] ? new Date(process.argv[2]).getTime() : null
+const end_date = process.argv[3] ? new Date(process.argv[3]).getTime() : null
 
 require('./index.js').run({ start_date, end_date })
--- back/src/module/publishAllBatch/index.js
+++ back/src/module/publishAllBatch/index.js
@@ -1,19 +1,22 @@
 import { run as publishBatch } from '../publishBatch'
 
 type Options = {
-    start_date?: number,
-    end_date?: number,
+    start_date?: number | null,
+    end_date?: number | null,
 }
 
 const DAY = 1000 * 60 * 60 * 24
 
 export const run = async (options?: Options = {}) => {
-
     const end_date = options.end_date || Date.now()
-    const start_date = options.start_date || ( end_date - DAY * 30 )
+    const start_date = options.start_date || end_date - DAY * 30
 
-    console.log(`publish batch between ${ new Date(start_date).toISOString() } - ${ new Date(end_date).toISOString() }`)
+    console.log(
+        `publish batch between ${new Date(
+            start_date
+        ).toISOString()} - ${new Date(end_date).toISOString()}`
+    )
 
-    for( let date = start_date ; date < end_date ; date += DAY )
+    for (let date = start_date; date < end_date; date += DAY)
         await publishBatch({ date })
 }
